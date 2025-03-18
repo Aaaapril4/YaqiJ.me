a5a74c5ef1f47eeecd59781f5112691e8c7c336a
Aaaapril4 <gia.jyq@gmail.com>
2025-03-18 03:29:31 -0400

    modify contacts

--- yaqij-home/app/components/contacts.js
+++ yaqij-home/app/components/contacts.js
@@ -1,26 +1,31 @@
 'use client';
 
 import Link from 'next/link';
-import { SiGithub, SiLinkedin } from "react-icons/si";
-import { TfiEmail } from "react-icons/tfi";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { FaGoogleScholar, FaGithub, FaLinkedin } from "react-icons/fa6";
 
 const Contacts = ({ className }) => {
 
     return (
-        <div className={`${className || ''} flex flex-row justify-evenly`}>
+        <div className={`${className || ''} flex flex-row gap-12 justify-center`}>
             <div key={'email'}>
-                <Link href={''}>
-                    <TfiEmail size={40}/>
+                <Link href={'mailto:april.jyq@gmail.com'} target="_blank" rel="noopener noreferrer">
+                    <MdOutlineAlternateEmail size={40}/>
                 </Link>
             </div>
             <div key={'github'}>
-                <Link href={''}>
-                    <SiGithub size={40}/>
+                <Link href={'https://github.com/Aaaapril4'} target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={40}/>
+                </Link>
+            </div>
+            <div key={'googlescholar'}>
+                <Link href={'https://scholar.google.com/citations?user=X1SoTQcAAAAJ'} target="_blank" rel="noopener noreferrer">
+                    <FaGoogleScholar size={40}/>
                 </Link>
             </div>
             <div key={'linkedin'}>
-                <Link href={''}>
-                    <SiLinkedin size={40}/>
+                <Link href={'https://www.linkedin.com/in/yaqi-jie'} target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={40}/>
                 </Link>
             </div>
         </div>
