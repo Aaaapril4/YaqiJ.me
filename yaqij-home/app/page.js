5c577ef81fbf773f5f0d6d7c548d634bd049f0a4
Aaaapril4 <gia.jyq@gmail.com>
2025-03-22 17:23:22 -0400

    add layout for another half

--- yaqij-home/app/page.js
+++ yaqij-home/app/page.js
@@ -4,16 +4,25 @@ import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
 import Side from "@/app/components/side";
+import Intro from "@/app/components/Intro";
 
 export default function Home() {
   return (
     <Theme accentColor="[#FFFEFB]" grayColor="slate" className="text-[#27272A]">
-      <main className="min-h-screen">
-        <Side />
-      </main>
-      <footer className="fixed bottom-0 w-full text-center p-2 text-sm text-gray-500 bg-[#FFFEFB] z-[51]">
+      <div className="flex min-h-screen">
+        <div className="w-1/2">
+          <Side />
+        </div>
+
+        <div className="w-1/2 min-h-screen p-8 flex justify-center items-start overflow-y-auto bg-[#FFFEFB] relative">
+          <div className='relative grid grid-cols-6 w-full gap-4 max-w-xl p-4 lg:max-w-4xl xl:grid-cols-12'>
+          </div>
+        </div>
+
+        <footer className="fixed bottom-0 w-full text-center p-2 text-sm text-gray-500 bg-[#FFFEFB] z-[51]">
           © {new Date().getFullYear()} Yaqi Jie | MIT License | Source code: <a href="https://github.com/Aaaapril4/YaqiJ.me" className="text-blue-500 hover:text-blue-600">https://github.com/Aaaapril4/YaqiJ.me</a>
-      </footer>
+        </footer>
+      </div>
     </Theme>
   );
 }
