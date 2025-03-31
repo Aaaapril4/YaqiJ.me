7ade18717452bb1e0e20c2c53fd753512a49068b
Aaaapril4 <gia.jyq@gmail.com>
2025-03-31 19:38:12 -0400

    make scroll-bar invisible

--- yaqij-home/app/ui/popup.js
+++ yaqij-home/app/ui/popup.js
@@ -30,11 +30,11 @@ const Popup = ({ trigger, content, className }) => {
             </div>
         </PopoverTrigger>
         <PopoverContent>
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl shadow-xl bg-[#FFFFFD] max-w-[900px] h-[90%] w-[90%] animate-in fade-in-0 zoom-in-90 duration-300 ease-out">
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl shadow-xl bg-[#FFFFFD] max-w-[900px] h-auto max-h-[90%] w-[90%] animate-in fade-in-0 zoom-in-90 duration-300 ease-out">
                 <PopoverClose className="absolute right-2 top-2 opacity-70 hover:opacity-100 transition-all duration-300 ease-out hover:scale-110">
                     <IoClose size={40} className="hover:scale-103 hover:drop-shadow-lg" />
                 </PopoverClose>
-                <div className="overflow-y-auto max-h-[80vh] my-10 mx-4">
+                <div className="overflow-y-auto max-h-[80vh] my-10 mx-4 scrollbar-hide">
                 {content}
                 </div>
             </div>
