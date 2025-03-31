18ba1835a0c0f5d45a585bef0f95ea991f630c5c
Aaaapril4 <gia.jyq@gmail.com>
2025-03-30 23:57:17 -0400

    Finish popup

--- yaqij-home/app/page.js
+++ yaqij-home/app/page.js
@@ -5,6 +5,7 @@ import { Theme } from "@radix-ui/themes";
 
 import Side from "@/app/components/side";
 import Intro from "@/app/components/Intro";
+import { Popup } from "@/app/ui/popup";
 
 export default function Home() {
   return (
@@ -20,7 +21,19 @@ export default function Home() {
 
         <div className="w-1/2 min-h-screen overflow-y-auto p-8 flex justify-center items-start bg-[#FFFEFB] text-[#27272A] relative">
           <div className='relative grid grid-cols-6 w-full gap-4 p-4 max-w-xl lg:max-w-4xl xl:grid-cols-12'>
-            <Intro className="content-fade-in" />
+            
+            <Popup 
+              className="col-span-6"
+              trigger={<Intro className="content-fade-in" />}
+              content={
+                <div className="p-4">
+                  <h2 className="text-2xl font-bold mb-4">About Me</h2>
+                  <p>This is a popup content example.</p>
+                  <a href="https://github.com/Aaaapril4/YaqiJ.me" className="text-blue-500 hover:text-blue-600">https://github.com/Aaaapril4/YaqiJ.me</a>
+                </div>
+              }
+            />
+            
           </div>
         </div>
 
---
+++ yaqij-home/app/ui/popup.js
@@ -0,0 +1,46 @@
+import * as React from "react"
+import * as PopoverPrimitive from "@radix-ui/react-popover"
+import { IoClose } from "react-icons/io5";
+
+const Popover = PopoverPrimitive.Root
+const PopoverTrigger = PopoverPrimitive.Trigger
+const PopoverAnchor = PopoverPrimitive.Anchor
+const PopoverContent = React.forwardRef(({ className, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+        <PopoverPrimitive.Content
+        ref={ref}
+        className={`
+            relative inset-0 z-50 h-screen w-screen blur-filter ${className}`}
+        {...props} />
+    </PopoverPrimitive.Portal>
+));
+PopoverContent.displayName = PopoverPrimitive.Content.displayName
+
+const PopoverClose = PopoverPrimitive.Close
+
+const Popup = ({ trigger, content, className }) => {
+  return (
+    <Popover>
+        <PopoverAnchor asChild>
+            <div className="fixed w-full top-0 left-0 invisible" />
+        </PopoverAnchor>
+        <PopoverTrigger asChild>
+            <div className={`cursor-pointer ${className}`}>
+                {trigger}
+            </div>
+        </PopoverTrigger>
+        <PopoverContent>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl shadow-xl bg-[#FFFFFD] max-w-[800px] h-[80%] w-[80%] animate-in fade-in-0 zoom-in-90 duration-300 ease-out">
+                <PopoverClose className="absolute right-2 top-2 opacity-70 hover:opacity-100 transition-all duration-300 ease-out hover:scale-110">
+                    <IoClose size={40} className="hover:scale-103 hover:drop-shadow-lg" />
+                </PopoverClose>
+                <div className="overflow-y-auto max-h-[80vh] my-10 mx-4">
+                {content}
+                </div>
+            </div>
+        </PopoverContent>
+    </Popover>
+  )
+}
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverClose, Popup }
