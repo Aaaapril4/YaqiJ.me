e6850d8d547d485bbb34a1494c990be8bff769d4
Aaaapril4 <gia.jyq@gmail.com>
2025-04-01 00:14:57 -0400

    fix a bug

--- yaqij-home/app/components/catchup.js
+++ yaqij-home/app/components/catchup.js
@@ -7,7 +7,7 @@ const Catchup = ({ className }) => {
             <div className="flex flex-col justify-center items-center p-12">
                 <div className="mt-4 transition-colors duration-300">
                     <span className="inline-block group-hover:hidden"><MdTipsAndUpdates size={40} className="group-hover:scale-103 group-hover:drop-shadow-lg"/></span>
-                    <span className="hidden group-hover:inline-block text-sm group-hover:text-[#27272A]">I'm looking for a full-time engineer role.</span>
+                    <span className="hidden group-hover:inline-block text-sm group-hover:text-[#27272A]">I&apos;m looking for a full-time engineer role.</span>
                 </div>
             </div>
         </Block>
